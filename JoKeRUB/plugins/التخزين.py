71ec662b233aa590563d00c27c14f805eefe01b4
Husam <a8919.8.3.2@googlemail.com>
2024-10-20 17:11:48 +0300

    Update التخزين.py

--- JoKeRUB/plugins/التخزين.py
+++ JoKeRUB/plugins/التخزين.py
@@ -116,12 +116,12 @@ async def log_tagged_messages(event):
 
 
 @l313l.ar_cmd(
-    pattern="خزن(?:\s|$)([\s\S]*)",
-    command=("خزن", plugin_category),
+    pattern="خاص(?:\s|$)([\s\S]*)",
+    command=("خاص", plugin_category),
     info={
         "header": "To log the replied message to bot log group so you can check later.",
         "الاسـتخـدام": [
-            "{tr}خزن",
+            "{tr}خاص",
         ],
     },
 )
@@ -130,11 +130,11 @@ async def log(log_text):
     if BOTLOG:
         if log_text.reply_to_msg_id:
             reply_msg = await log_text.get_reply_message()
-            await reply_msg.forward_to(BOTLOG_CHATID)
+            await reply_msg.forward_to("me")
         elif log_text.pattern_match.group(1):
             user = f"#التخــزين / ايـدي الدردشــه : {log_text.chat_id}\n\n"
             textx = user + log_text.pattern_match.group(1)
-            await log_text.client.send_message(BOTLOG_CHATID, textx)
+            await log_text.client.send_message(me, textx)
         else:
             await log_text.edit("**⌔┊بالــرد على اي رسـاله لحفظهـا في كـروب التخــزين**")
             return
