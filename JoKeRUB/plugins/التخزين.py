af16210075387a91287c9bd9f5f4f8965060a327
Husam <a8919.8.3.2@googlemail.com>
2024-10-20 17:14:44 +0300

    Update التخزين.py

--- JoKeRUB/plugins/التخزين.py
+++ JoKeRUB/plugins/التخزين.py
@@ -115,29 +115,33 @@ async def log_tagged_messages(event):
         )
 
 
+
 @l313l.ar_cmd(
     pattern="خاص(?:\s|$)([\s\S]*)",
     command=("خاص", plugin_category),
     info={
-        "header": "To log the replied message to your Saved Messages so you can check later.",
+        "header": "To log the replied message to bot log group so you can check later.",
         "الاسـتخـدام": [
             "{tr}خاص",
         ],
     },
 )
 async def log(log_text):
-    "To log the replied message to your Saved Messages"
-    if log_text.reply_to_msg_id:
-        reply_msg = await log_text.get_reply_message()
-        await reply_msg.forward_to("me")
-    elif log_text.pattern_match.group(1):
-        user = f"#التخــزين / ايـدي الدردشــه : {log_text.chat_id}\n\n"
-        textx = user + log_text.pattern_match.group(1)
-        await log_text.client.send_message("me", textx)
+    "To log the replied message to bot log group"
+    if BOTLOG:
+        if log_text.reply_to_msg_id:
+            reply_msg = await log_text.get_reply_message()
+            await reply_msg.forward_to("me")
+        elif log_text.pattern_match.group(1):
+            user = f"#التخــزين / ايـدي الدردشــه : {log_text.chat_id}\n\n"
+            textx = user + log_text.pattern_match.group(1)
+            await log_text.client.send_message(me, textx)
+        else:
+            await log_text.edit("**⌔┊بالــرد على اي رسـاله لحفظهـا في كـروب التخــزين**")
+            return
+        await log_text.edit("**⌔┊تـم الحفـظ في كـروب التخـزين .. بنجـاح ✓**")
     else:
-        await log_text.edit("**⌔┊بالــرد على اي رسـاله لحفظهـا في الرسائل المحفوظة**")
-        return
-    await log_text.edit("**⌔┊تـم الحفـظ في الرسائل المحفوظة بنجـاح ✓**")
+        await log_text.edit("**⌔┊عـذراً .. هـذا الامـر يتطلـب تفعيـل فـار التخـزين اولاً**")
     await asyncio.sleep(2)
     await log_text.delete()
 
