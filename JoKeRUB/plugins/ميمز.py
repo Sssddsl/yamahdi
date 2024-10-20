d884336ade03eeeb47324989276261ad27ffb7ea
Husam <a8919.8.3.2@googlemail.com>
2024-10-20 03:36:06 +0300

    Update ميمز.py

--- JoKeRUB/plugins/ميمز.py
+++ JoKeRUB/plugins/ميمز.py
@@ -395,7 +395,7 @@ async def jepmeme(memejep):
 @l313l.on(admin_cmd(outgoing=True, pattern="يدكتور$"))
 async def jepmeme(memejep):
   Jep = await reply_id(memejep)
-  url = f"https://t.me/vipabh/1110"
+  url = f"https://t.me/vipabh/1107"
   await memejep.client.send_file(memejep.chat_id,url,caption="",parse_mode="html",reply_to=Jep)
   await memejep.delete()
 @l313l.on(admin_cmd(outgoing=True, pattern="ميم"))
