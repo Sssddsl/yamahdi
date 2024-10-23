c23eae745fd06cc7f28398f457f96ab84a4f3aee
Husam <a8919.8.3.2@googlemail.com>
2024-10-23 16:49:31 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,11 +1,11 @@
 from sample_config import Config
 class Development(Config):
     APP_ID = 
-    API_HASH = "
+    API_HASH = ""
     ALIVE_NAME = "•"
     DB_URI = "postgresql://postgres:your_password@localhost:5432/aljoke"
-    STRING_SESSION = "
-    TG_BOT_TOKEN = "
+    STRING_SESSION = ""
+    TG_BOT_TOKEN = ""
     COMMAND_HAND_LER = "."
     SUDO_USERS = []
     SUDO_COMMAND_HAND_LER = "."
