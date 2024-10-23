4f9449134a2dc07e16fe8e7aa8d719bc9e9aac2f
Husam <a8919.8.3.2@googlemail.com>
2024-10-24 00:23:21 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,11 +1,11 @@
 from sample_config import Config
 class Development(Config):
-    APP_ID = 
-    API_HASH = ""
+    APP_ID = 22792746
+    API_HASH = "4b6f9f5ab64dd8600044bb95c9ed02c6"
     ALIVE_NAME = "•"
     DB_URI = "postgresql://postgres:your_password@localhost:5432/aljoke"
-    STRING_SESSION = ""
-    TG_BOT_TOKEN = ""
+    STRING_SESSION = "1ApWapzMBu7Qe8k0GwDdc4RP5VYQqVFTBNf6FtVULkElHZ9fBKwHDVsQAGerGyfscwis6rh6VuXtYt47AmI-QdBIGya8IE_rosCtq1e-0VrmyLg0zN26XoA0M9_spErJw0KfcogmxusJxBoKchj1-EeBbsxvROxC2w4dGU_Kkpv8jnQaR-F-d75f6gS_5kJdlxr1F8nAh3_9IfRQsgxpRx4BDcJZhT9HhA91wNvI5QcsEiTw7tiLydlZmz1Vm2iOdRbzbQ6CCOzut9GZcuGYurDg4sdxQEB4ygEAdIpBGxeN4Drk_RTKS5DjguI9T0fWF_M3wlfPx4VreEWo-yOhHPqKTQ519swg="
+    TG_BOT_TOKEN = "7310855662:AAEeMlFLeTjQduKSeWCUm70UPVdUMfbyjaI"
     COMMAND_HAND_LER = "."
     SUDO_USERS = []
     SUDO_COMMAND_HAND_LER = "."
